e9906bbdffd14ca6405097e305a472b6cad3b4db
eunoo1995 <eunoo1995@gmail.com>
2023-02-04 19:49:10 +0900

    feat. add authToken utils
    토큰 유틸 함수 추가

--- src/lib/jwt/authToken.js
+++ src/lib/jwt/authToken.js
@@ -1,9 +1,7 @@
-const getAuthToken = () => {
+export const getAuthToken = () => {
   return localStorage.getItem('ACCESS_TOKEN');
 };
 
-const setAuthToken = token => {
+export const setAuthToken = token => {
   localStorage.setItem('ACCESS_TOKEN', token);
 };
-
-export { getAuthToken, setAuthToken };
