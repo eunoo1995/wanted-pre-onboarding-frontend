15135591dc9a21e76fa832e5aebbf27995a0a373
eunoo1995 <eunoo1995@gmail.com>
2023-02-05 13:36:08 +0900

    feat. implement SignIn/SignUp page component
    로그인/회원가입 컴포넌트 구현

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,3 +1,15 @@
+import { SignUpForm } from '../containers';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getAuthToken } from '../lib/jwt/authToken';
+
 export const SignUpPage = () => {
-  return <div>회원가입</div>;
+  const navigate = useNavigate();
+  const token = getAuthToken();
+
+  useEffect(() => {
+    if (token) navigate('/todo');
+  });
+
+  return <>{!token && <SignUpForm />}</>;
 };
