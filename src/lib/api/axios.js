f39ed34283d44d6c48592514bfa9f23517574c6c
eunoo1995 <eunoo1995@gmail.com>
2023-02-05 19:00:59 +0900

    fix. edit api return value
    리턴 값 오류 수정

--- src/lib/api/axios.js
+++ src/lib/api/axios.js
@@ -5,4 +5,8 @@ axios.defaults.baseURL = 'https://pre-onboarding-selection-task.shop/';
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${getAuthToken()}`;
 
+export const setHeader = token => {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+};
+
 export default axios;
